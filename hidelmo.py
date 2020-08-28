4e5ec3107419778cbb9fd3d20ed1547e73281600
Khaled ElMorshedy <elmoiv@yahoo.com>
2020-08-28 14:45:13 +0200

    Update some code

--- hidelmo.py
+++ hidelmo.py
@@ -9,89 +9,62 @@ Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrom
 Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36
 Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36
 Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36
-Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36
-Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36
-Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36
-Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10
-Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36
-Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36
-Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36'''
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'''
 
 def hideMe(url, filename):
     data = {
         "u":url,
-        "encodeURL":"1",
-        #"proxy_formdata_server":"nl",
-        #"stripJS":"0",
-        #"stripObjects":"0",
-        #"go":"",
-        #"encodePage":"0",
-        #"allowCookies":"1"
+        "encodeURL":"1"
     }
     headers = {
-        "user-agent":random.choice(userAgents.split('\n')),
-        #"content-type":"application/x-www-form-urlencoded",
-        #"sec-fetch-mode":"navigate",
-        #"sec-fetch-user":"?1",
-        #"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        #"Upgrade-Insecure-Requests":"1",
-        #"dnt":"1",
-        #"origin":"https://hide.me",
-        #"referer":"https://hide.me/en/proxy",
-        #"accept-encoding": "gzip, deflate, br",
-        #"accept-language": "en-US,en;q=0.9,ar;q=0.8,ca;q=0.7"
+        "user-agent":random.choice(userAgents.split('\n'))
     }
 
-
-    # content type should anything but html
+    # Content type should anything but html
     d = "text/html; charset=UTF-8"
     n = 0
+    
     # Connecting in a loop till our death or server responds lol.
     with open(filename, 'wb') as f:
         while(d == "text/html; charset=UTF-8"):
             n += 1
+            print(f"Connecting to Hide.me... {n}")
+            
             response = requests.post(
                 "https://nl.hideproxy.me/includes/process.php?action=update",
                 data=data,
                 headers=headers,
-                stream=True)
+                stream=True
+                )
 
             d = response.headers.get('content-type')
 
-            print(f"Trying... {n}")
-            #print(response.headers)
         total = response.headers.get('content-length')
-
         if total is None:
             f.write(response.content)
-        
         else:
             downloaded = 0
             total = int(total)
 
-            for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
-                downloaded += len(data)
+            for data in response.iter_content(chunk_size=max(int(total / 1000), 1 << 20)):
                 f.write(data)
-                done = int(50*downloaded/total)
-                file_size = round(total / (1024**2))
-                file_down = round(downloaded / (1024**2))
-                sys.stdout.write('\r[{}{}] [{}/{} MB]'.format('█' * done, ' ' * (50-done), file_down, file_size))
-                sys.stdout.flush()
+                downloaded += len(data)
+                percent = int(50 * downloaded / total)
+                r_size = total / (1 << 20)
+                d_size = downloaded / (1 << 20)
+                print(
+                    '\r[{}{}] '.format(
+                                    '█' * percent,
+                                    ' ' * (50 - percent)
+                                       ) \
+                    + '[{0:.2f}/{1:.2f} MB]'.format(
+                                        d_size,
+                                        r_size
+                                        ),
+                    end='\r')
 
-    sys.stdout.write('\n')
+    print()
 
-while True:hideMe(input("\nurl: "), input("name: "))
+if __name__ == '__main__':
+    url, name = sys.argv[1:3]
+    hideMe(eval(f'"{url}"'), name)
